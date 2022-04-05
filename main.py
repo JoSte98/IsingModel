c04e5b8f166a5dcb49631dd19413fd9ce0713aaf
MagPar <majda.paryzkova@seznam.cz>
2022-04-05 18:06:31 +0200

    Theoretical results for magnetization added to the plot.

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
     :return: 0 if successful
     """
 
-    """for T in np.arange(1, 4.01, 0.2):
+    for T in np.arange(1, 4.01, 0.2):
         T = np.round(T, 2)
         model = IsingModel(temperature=T, length=50, init_type='close_to_opt')
 
@@ -28,13 +28,13 @@ def main():
         save_temp_magn(T, model.final_magnetization, model.final_magnetization_sigma, NAME_PHASE_FILE)
         save_temp_energy(T, model.final_energy, model.final_energy_sigma, NAME_ENERGY_FILE)
         save_temp_susceptibility(T, model.final_susceptibility, model.final_susceptibility_sigma, NAME_SUSCEPTIBILITY_FILE)
-        save_temp_specific_heat(T, model.final_specific_heat, model.final_specific_heat_sigma, NAME_SPECIFIC_HEAT_FILE)"""
+        save_temp_specific_heat(T, model.final_specific_heat, model.final_specific_heat_sigma, NAME_SPECIFIC_HEAT_FILE)
 
-    #plot_temp_corr_time(NAME_CORR_FILE)
+    plot_temp_corr_time(NAME_CORR_FILE)
     plot_phase_diagram(NAME_PHASE_FILE)
-    #plot_temp_energy(NAME_ENERGY_FILE)
-    #plot_temp_specific_heat(NAME_SPECIFIC_HEAT_FILE)
-    #plot_temp_susceptibility(NAME_SUSCEPTIBILITY_FILE)
+    plot_temp_energy(NAME_ENERGY_FILE)
+    plot_temp_specific_heat(NAME_SPECIFIC_HEAT_FILE)
+    plot_temp_susceptibility(NAME_SUSCEPTIBILITY_FILE)
 
 
     return 0
